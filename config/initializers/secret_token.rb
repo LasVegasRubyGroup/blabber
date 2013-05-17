9be3cb161c17c3a2fe06d453b28cd029ab017e68
Fred Guest <fredguest@mac.com>
2013-05-16 21:47:19 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blabber::Application.config.secret_token = '5cdc17a1904f74028cf3f0156dab2a0fdc1d99d91df5366c15e68350bc0082c00218bdbb0485ec92e1cfb49990894ff569676025046f08d156371a061dd7baeb'
