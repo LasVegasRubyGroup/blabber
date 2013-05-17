143edce2afdd0bec15e6b0d782fadc4dee3d492f
Fred Guest <fredguest@mac.com>
2013-05-17 00:06:44 -0700

    add blab views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation
 	validates_uniqueness_of :email
 	has_secure_password
-	has_many_blabs
+	has_many :blabs
 end
